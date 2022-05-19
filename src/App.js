f0495d0d9ce52d3ac213f167321c5bd5247c381a
twotimesgi <luigibardellagerbi@mail.com>
2022-05-19 19:58:59 +0200

    search posts

--- src/App.js
+++ src/App.js
@@ -1,77 +1,97 @@
 import './App.css';
-import {Container, Row, Col, Stack} from 'react-bootstrap';
+import { Container, Row, Col, Stack, Form } from 'react-bootstrap';
 import Post from './components/Post';
 import NewPost from './components/NewPost';
 import ProfilePic from './components/ProfilePic';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  let examplePosts = [
+  let DUMMY_POSTS = [
     {
-      user: 0,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum labo numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum.",
-      date: "1/19/2022 4:35:07 PM"
+      user: 3,
+      text: "If at first you don’t succeed, try, try again.	",
+      date: "5/20/2022 10:43:05 AM"
     },
     {
       user: 1,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum labo numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum.",
-      date: "2/01/2022 7:40:00 PM"
+      text: "If you are going through hell, keep going.	",
+      date: "02/01/2022 7:40:00 PM"
+    },
+    {
+      user: 0,
+      text: "I'm gonna make him an offer he can't refuse.	",
+      date: "01/19/2022 4:35:07 PM"
     },
     {
       user: 2,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum labo numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum.",
-      date: "5/19/2022 9:16:03 AM"
+      text: "Keep your friends close, but your enemies closer.	",
+      date: "05/19/2022 9:16:03 AM"
     },
   ]
-
-  const [postList, setPostList] = useState(examplePosts);
+  const postList = DUMMY_POSTS;
+  const [showedList, setShowedList] = useState(DUMMY_POSTS);
 
   const handleNewPost = (newPost) => {
-    setPostList([newPost, ...postList]);
+    postList.unshift(newPost);
+    setShowedList(postList);
+  }
+  const filterInput = React.createRef();
+  const filterPosts = () =>{
+    let filtered = postList.filter(post => post.text.trim().toLowerCase().includes(filterInput.current.value.trim().toLowerCase()));
+    setShowedList(filtered);
   }
 
   let users = [
     {
       user: "twotimesgi",
       img: require('./images/avatar1.jpg'),
-      newMessages: 0
+      newMessages: 10
     },
     {
       user: "pippotek",
       img: require('./images/avatar2.jpg'),
-      newMessages: 4
+      newMessages: 0
     },
     {
       user: "rusky99",
       img: require('./images/avatar3.jpg'),
       newMessages: 1
+    },
+    {
+      user: "beatrix7",
+      img: require('./images/avatar4.jpg'),
+      newMessages: 3
     }
   ]
 
-  let currentUser = 1;
+  // Switch profile here
+  let currentUser = 0;
 
-return (
-  <Container fluid="md">
-    <Row className='p-4'>
-      <Col className='d-flex justify-content-between'>
-        <NewPost currentUser={currentUser} user={users[currentUser]} onPost={handleNewPost}></NewPost>
-        <div>
+  return (
+    <Container fluid="md">
+      <Row className='p-4'>
+        <Col className='d-flex justify-content-start align-items-center'>
+          <NewPost currentUser={currentUser} user={users[currentUser]} onPost={handleNewPost}></NewPost>
+        </Col>
+        <Col className='d-flex justify-content-center align-items-center'>
+            <Form.Control ref={filterInput} className="form-control mr-sm-2" type="search" placeholder="Search" onChange={filterPosts} aria-label="Search posts"/>
+        </Col>
+        <Col className='d-flex justify-content-end align-items-center'>
           <ProfilePic user={users[currentUser]}></ProfilePic>
           <span>{users[currentUser].user}</span>
-        </div>
-      </Col>
-    </Row>
-    <Row className='p-4'>
-      { postList.length !== 0 ? (
-      <Stack gap={4} className="col-12 mx-auto">
-        {postList.map((postObj, index) => <Post key={index} user={users[postObj.user]} postObj={postObj} />)}
-      </Stack>
-      ) : (
-        <div className='py-5 text-center text-muted'>There are no posts</div>
-      ) }
-    </Row>
-  </Container>
-);
+        </Col>
+      </Row>
+      <Row className='p-4'>
+        {showedList.length !== 0 ? (
+          <Stack gap={4} className="col-12 mx-auto">
+            { showedList.map((postObj, index) => <Post key={index} user={users[postObj.user]} postObj={postObj} />)}
+          </Stack>
+        ) : (
+          <div className='py-5 text-center text-muted'>There are no posts</div>
+        )}
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
